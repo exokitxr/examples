80f75288d4e90837de09d97ee01113cc5a55f141
chrisplatorres <me@chrislatorres.com>
2019-08-22 21:07:06 -0500

    add examples

---
+++ launcher/build/precache-manifest.9d83b7d93fa41697da6e6e53ab27e88d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906ae9faceb196155e543247d9865683",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1efdd418ce091e612f5c",
+    "url": "./static/css/main.9d882275.chunk.css"
+  },
+  {
+    "revision": "efb905ce76ea91a23253",
+    "url": "./static/js/2.a1aac253.chunk.js"
+  },
+  {
+    "revision": "1efdd418ce091e612f5c",
+    "url": "./static/js/main.acbbee80.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
